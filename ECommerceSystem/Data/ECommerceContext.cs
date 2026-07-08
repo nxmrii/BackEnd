0a638831a7958172efc748e257dbf8ff0ecf3906
nxmrii <nalaamri19@gmail.com>
2026-07-08 16:39:06 +0400

    EF & Migration Done :)

---
+++ ECommerceSystem/Data/ECommerceContext.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ECommerceSystem.Data
+{
+
+
+    public class ECommerceContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            if (!option.IsConfigured)
+            {
+                option.UseSqlServer("Server=localhost;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+
+
+}
